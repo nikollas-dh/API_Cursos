06b7fbadbf235f851f6c85383e4a9803d9bed442
= <nikollasdheyvis17@gmail.com>
2026-05-18 08:34:07 -0300

    listar periodos

--- src/main/java/com/cursos/API_REST/controller/periodoController.java
+++ src/main/java/com/cursos/API_REST/controller/periodoController.java
@@ -1,35 +1,19 @@
 package com.cursos.API_REST.controller;
 
-import com.cursos.API_REST.Cursos.DadosListagemCurso;
 import com.cursos.API_REST.periodo.Periodo;
-import com.cursos.API_REST.periodo.PeriodoRepository;
-import org.springdoc.core.annotations.ParameterObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @RequestMapping("cursos/periodos")
 public class periodoController {
 
-    @Autowired
-    private PeriodoRepository periodoRepository;
+
 
     @GetMapping
-    public ResponseEntity<List<String>> listarPeriodo() {
-        var periodos = Arrays.stream(Periodo.values())
-                .map(Enum::name)
-                .toList();
-
-        return ResponseEntity.ok(periodos); // Retorna 200 OK com a lista
+    public Periodo[] listarTodos() {
+        // Retorna ["Matutino", "Vespertino", "Noturno", "Integral"]
+        return Periodo.values();
     }
 }
